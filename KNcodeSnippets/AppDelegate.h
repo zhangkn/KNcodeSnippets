a77665137877de4440f3ec492cba5b6ada4c9141
张坤楠 <zhangkunnan@qinbaowan.com>
2018-01-21 09:53:18 +0800

    Initial Commit

---
+++ KNcodeSnippets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNcodeSnippets
+//
+//  Created by devzkn on 21/01/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
