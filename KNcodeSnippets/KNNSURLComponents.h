cfb2eee054c7ebc8c3352bea27600fb51fe082f6
张坤楠 <zhangkunnan@qinbaowan.com>
2018-02-09 13:48:09 +0800

    使用NSURLComponents

---
+++ KNcodeSnippets/KNNSURLComponents.h
@@ -0,0 +1,13 @@
+//
+//  KNNSURLComponents.h
+//  KNcodeSnippets
+//
+//  Created by devzkn on 09/02/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNNSURLComponents : NSObject
+
+@end
---
+++ KNcodeSnippets/KNNSURLComponents.m
@@ -0,0 +1,35 @@
+//
+//  KNNSURLComponents.m
+//  KNcodeSnippets
+//
+//  Created by devzkn on 09/02/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import "KNNSURLComponents.h"
+
+@implementation KNNSURLComponents
+    
+    
+    
+    /**
+    使用NSURLComponents 进行 url请求 ge参数的构建
+     */
+    + (void)setupNsUrl{
+        
+        NSURLComponents *components = [NSURLComponents componentsWithString:@"url"];
+        
+        NSURLQueryItem * v = [NSURLQueryItem queryItemWithName:@"v" value:@"2"];
+
+        [components setQueryItems:@[v]];
+        
+        NSURL* url = [components URL];
+
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
+                                                               cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                           timeoutInterval:30];
+        
+        
+    }
+
+@end
